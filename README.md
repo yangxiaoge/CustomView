6ab4507d651afe4cba82f6481d5915a797a4486b
yang.jianan <364804051@qq.com>
2016-03-24 19:35:06 +0800

    小细节

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # CustomView
 自定义布局.练习.
 
-#Add  CustomView.java
+# Add  CustomView
  一个最简单的自定义view
 
 #Add TitleViewCombox
--- app/src/main/java/customview/example/com/customview/MainActivity.java
+++ app/src/main/java/customview/example/com/customview/MainActivity.java
@@ -1,13 +1,25 @@
 package customview.example.com.customview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import customview.example.com.customview.widget.CountView;
+import customview.example.com.customview.widget.TitleViewCombox;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.count_view)
+    CountView countView;
+    @Bind(R.id.titleview_combox)
+    TitleViewCombox titleViewCombox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
+
 }
